26fbd225a5dff40c6a897a57d04000fd5064435d
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2019-01-29 07:13:16 -0500

    git push

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -12,11 +12,18 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/demoDaydb"
+,{ useNewUrlParser: true} );
+
 
 // Start the API server
 app.listen(PORT, function() {
